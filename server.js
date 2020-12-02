376bea5e12bba6c265cb9a55c8ce7307129f0233
jojokimys <jojokimys@users.noreply.github.com>
2020-12-02 21:24:46 +0900

    server

--- server.js
+++ server.js
@@ -12,7 +12,8 @@ const port = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static('./'));
+app.use(express.static('./public'));
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
